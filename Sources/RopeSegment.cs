518dcc3b259aab58b52c067ce49161d59da1d8bb
romaster28 <sony.ba6@gmail.com>
2025-10-14 00:19:56 +0300

    First commit

---
+++ Sources/RopeSegment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct RopeSegment
+{
+    public Vector3 Position { get; set; }
+    public Vector3 Old { get; set; }
+
+    public RopeSegment(Vector3 position)
+    {
+        Position = position;
+        Old = position;
+    }
+}
